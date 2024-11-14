648b45b27a687298f136e1b2eadaa05d710d5045
Mohammed Hassan1999 <120308228+MohammedHussein1999@users.noreply.github.com>
2024-11-14 22:46:06 +0200

    first commit Last

--- resources/views/collections/cities.blade.php
+++ resources/views/collections/cities.blade.php
@@ -276,7 +276,7 @@
                                                             <th class=" text-white">رقم الغرفه</th>
                                                             <th class=" text-white">السعه</th>
                                                             <th class=" text-white">رقم الطابق</th>
-                                                            <th class="text-white"> التسكين</th>
+
                                                             <th class="text-white"> حذف الغرفة</th>
                                                         </tr>
                                                         @foreach ($unit->rooms as $room )
@@ -295,15 +295,7 @@
                                                                         class="font-bold text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 rounded-lg text-sm px-5 py-2.5">حذف</button>
                                                                 </form>
                                                             </td>
-                                                            <td>
-                                                                <a href="{{ route('notification.housingNew', ['id'=>$room->id]) }}">
-                                                                <button type="button"
-                                                                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-
-                                                                        تسكين
-                                                                    </button>
-                                                                </a>
-                                                            </td>
+                
                                                         </tr>
                                                         @endforeach
                                                     </table>
